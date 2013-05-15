baa46763a50dd5c112481adeae1217a8108d1e18
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2013-05-15 17:39:51 +0200

    New tests for new function reload

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var should = require('should');
 var loader = require('../lib/node-yaml-config');
 
 var file = __dirname + '/config.yml';
+var save_file = __dirname + '/config.yml.bak';
+var new_file = __dirname + '/config-reload.yml';
 
 var example_dev_config = {
   server: { 
@@ -43,6 +45,44 @@ var example_prod_config = {
   }
 };
 
+var example_new_dev_config = {
+  server: {
+    port: 8080
+  },
+  database: {
+    host: 'localhost',
+    port: 27017,
+    db: 'dev_db'
+  }
+};
+
+var example_new_test_config = {
+  server: {
+    port: 443
+  },
+  database: {
+    host: 'localhost',
+    port: 27017,
+    db: 'test_db'
+  }
+};
+
+var example_new_prod_config = {
+  server: {
+    port: 8000
+  },
+  database: {
+    host: 'localhost',
+    port: 27017,
+    db: 'prod_db',
+    user: 'dbuser',
+    password: 'pass'
+  },
+  cache: {
+    dir: 'cache'
+  }
+};
+
 describe('node-yaml-config', function() {
   describe('#load()', function() {
     describe('should corectly load the different configurations', function() {
@@ -59,11 +99,11 @@ describe('node-yaml-config', function() {
     describe('should not reread the file', function() {
 
       before(function() {
-        fs.renameSync(file, __dirname + '/../examples/config.yml.bak');
+        fs.renameSync(file, save_file);
       });
 
       after(function() {
-        fs.renameSync(__dirname + '/../examples/config.yml.bak', file);
+        fs.renameSync(save_file, file);
       });
 
       it('development', function() {
@@ -74,6 +114,37 @@ describe('node-yaml-config', function() {
       });
       it('production', function() {
         should.deepEqual(loader.load(file, 'production'), example_prod_config);
+      });
+    });
+  });
+
+  describe('#reload()', function() {
+    describe('should correctly load the new configurations', function() {
+
+      before(function() {
+        fs.renameSync(file, save_file);
+        fs.renameSync(new_file, file);
+
+        try {
+          loader.reload(file);
+        } catch (err) {
+          console.log(err);
+        }
+      });
+
+      after(function() {
+        fs.renameSync(file, new_file);
+        fs.renameSync(save_file, file);
+      });
+
+      it('development', function() {
+        should.deepEqual(loader.load(file, 'development'), example_new_dev_config);
+      });
+      it('test', function() {
+        should.deepEqual(loader.load(file, 'test'), example_new_test_config);
+      });
+      it('production', function() {
+        should.deepEqual(loader.load(file, 'production'), example_new_prod_config);
       });
     });
   });
